0383a1d8cfa04655659ba1440a902e5a07aa0538
Jianyuan <malinhmh199495@gmail.com>
2020-10-10 10:48:19 +0800

    logo

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-from sys import argv
+# from sys import argv
+from argparse import ArgumentParser
+from math import ceil
 
 import docx2pdf
 import fitz
 # import img2pdf
 from PIL import Image
-from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
-from fpdf import FPDF
+from PyPDF2 import PdfFileReader, PdfFileWriter  # , PdfFileMerger
+
+
+# from fpdf import FPDF
 
 
 def convert_doc_to_pdf(doc_file):
     # Use a breakpoint in the code line below to debug your script.
     # Press Ctrl+F8 to toggle the breakpoint.
     filename, _ = os.path.splitext(doc_file)
-    # print(filename)
     pdf_file = filename + '.pdf'
     docx2pdf.convert(doc_file, pdf_file)
-    # os.remove(doc_file)
 
     return pdf_file
 
 
 # def convert_image_to_pdf(image_file):
-#     # filename = '.'.join(image_file.split('.')[:-1])
 #     filename, _ = os.path.splitext(image_file)
 #     image = Image.open(image_file)
 #     if image.mode == 'RGBA':
 #         bg = Image.new('RGB', image.size, (255, 255, 255))
 #         bg.paste(image, (0, 0), image)
 #         bg.save('alpha_removed.png', 'PNG')
-#         # os.remove(image_file)
 #         image_file = 'alpha_removed.png'
 #
 #     pdf_file = filename + '.pdf'
@@ -42,112 +42,146 @@ def convert_doc_to_pdf(doc_file):
 #     return pdf_file
 
 
+def text_to_sequence(text, font):
+    max_lens = {16: 70, 14: 80, 12: 90}
+    max_len = max_lens[font]
+    words = text.split()
+    sequence = []
+    line = ''
+    while len(words) > 0:
+        word = words.pop(0)
+        if len(line) + len(word) < max_len:
+            line = ' '.join([line, word])
+        else:
+            sequence.append(line)
+            line = word
+    sequence.append(line)
+    return sequence
+
+
 def main():
-    if len(argv) == 2:
-        folder = argv[1]
-    else:
-        folder = '.\\PDF Working Data\\Sample 1'
+    # if len(argv) == 2:
+    #     folder = argv[1]
+    # else:
+    #     folder = '.\\PDF Working Data\\Sample 1'
+
+    parser = ArgumentParser()
+    parser.add_argument('--folder', type=str, default='.\\PDF Working Data\\Sample 2')
+    parser.add_argument('--logo', type=str, default='logo.png')
+    parser.add_argument('--title', type=str, default='Complete Subscription Package')
+    parser.add_argument('--sub_title', type=str, default='As Of October 9, 2020')
+    parser.add_argument('--comment', type=str,
+                        default='This is a paragraph which should appear after the sub-title, '
+                                'but before the start of the links/items in the table of contents'
+                        )
+
+    args = parser.parse_args()
+    folder = args.folder
+
+    # merger = PdfFileMerger()
+    # merge_list = []
     merged_file = os.path.join(folder, 'merged.pdf')
-
-    if os.path.exists(merged_file):
-        os.remove(merged_file)
-
     converted_files, image_files = [], []
-    merger = PdfFileMerger()
-    bookmarks, merge_list = [], []
+    bookmarks = []
     page = 0
+    docs = fitz.open()
 
     for file in os.listdir(folder):
-        # print(file)
         filename, extension = os.path.splitext(file)
         file = os.path.join(folder, file)
-        if extension in ['.doc', '.docx']:
-            file = convert_doc_to_pdf(file)
-            converted_files.append(file)
-            merge_list.append(file)
-            # merger.addBookmark(title=filename, pagenum=page)
-            bookmarks.append((filename, page + 1))
-            page += PdfFileReader(file).numPages
-        elif extension in ['.png', '.jpg']:
+        if extension in ['.png', '.jpg']:
             # file = convert_image_to_pdf(file)
             # converted_files.append(file)
             image_files.append(file)
         else:
-            merge_list.append(file)
-            # merger.addBookmark(title=filename, pagenum=page)
-            bookmarks.append((filename, page + 1))
-            page += PdfFileReader(file).numPages
-
-    blank_pdf = FPDF(unit='pt', format='letter')
-    for _ in range(len(image_files)):
-        blank_pdf.add_page()
-
-    blank_pdf.output('blank.pdf', 'F')
-
-    with fitz.open('blank.pdf') as document:
-        for i, file in enumerate(image_files):
+            if extension in ['.doc', '.docx']:
+                file = convert_doc_to_pdf(file)
+                converted_files.append(file)
             # merge_list.append(file)
-            blank_page = document[i]
-            image = Image.open(file)
-            width, height = image.size
+            # merger.append(file, import_bookmarks=False)
+            # merger.addBookmark(title=filename, pagenum=page)
+            # page += PdfFileReader(file).numPages
+            with fitz.open(file) as doc:
+                docs.insertPDF(doc)
+                bookmarks.append((filename, page + 1))
+                page += doc.pageCount
 
-            if height < width * 1.3:
-                x0, x1 = 115, 497
-                h = int(height * 382 / width)
-                y0 = int(396 - h / 2)
-                y1 = 792 - y0
-            else:
-                y0, y1 = 149, 643
-                w = int(width * 494 / height)
-                x0 = int(306 - w / 2)
-                x1 = 612 - x0
+    # merger.write(merged_file)
+    # merger.close()
 
-            img_rect = fitz.Rect(x0, y0, x1, y1)
-            blank_page.insertImage(img_rect, filename=file)
+    # docs = fitz.open(merged_file)
+    width, height = 612, 792
+    for i, file in enumerate(image_files):
+        image = Image.open(file)
+        image_width, image_height = image.size
 
-            filename, _ = os.path.splitext(file)
-            filename = filename.split('\\')[-1]
-            bookmarks.append((filename, page + 1))
-            page += 1
+        if image_height < image_width * 1.3:
+            x0, x1 = 115, 497
+            h = int(image_height * 382 / image_width)
+            y0 = int(396 - h / 2)
+            y1 = 792 - y0
+        else:
+            y0, y1 = 149, 643
+            w = int(image_width * 494 / image_height)
+            x0 = int(306 - w / 2)
+            x1 = 612 - x0
 
-        document.save('image.pdf')
+        img_rect = fitz.Rect(x0, y0, x1, y1)
+        docs.newPage(-1, width=width, height=height)
+        docs[page].insertImage(img_rect, filename=file)
 
-    os.remove('blank.pdf')
+        filename, _ = os.path.splitext(file)
+        filename = filename.split('\\')[-1]
+        bookmarks.append((filename, page + 1))
+        page += 1
 
-    toc = FPDF(unit='pt', format='letter')
-    toc.add_page()
-    toc.set_font('Arial')
-    x, y = 0, 36
+    docs.newPage(0, width=width, height=height)
+
+    # insert logo image
+    logo = Image.open(args.logo)
+    logo_width, logo_height = logo.size
+    x1, y0, y1 = 576, 36, 108
+    x0 = x1 - 72 * logo_width / logo_height
+    img_rect = fitz.Rect(x0, y0, x1, y1)
+    docs[0].insertImage(img_rect, filename=args.logo)
+
+    # insert title
+    y = 108
+    title_font, sub_title_font, comment_font = 16, 14, 12
+    title = text_to_sequence(text=args.title, font=title_font)
+    line_number = docs[0].insertText(point=fitz.Point(108, y), text=title, fontsize=title_font)
+
+    # insert subtitle
+    y += ceil(line_number / 2) * 36
+    sub_title = text_to_sequence(text=args.sub_title, font=sub_title_font)
+    line_number = docs[0].insertText(point=fitz.Point(144, y), text=sub_title, fontsize=sub_title_font)
+
+    # insert comment
+    y += ceil(line_number / 2) * 36
+    comment = text_to_sequence(text=args.comment, font=comment_font)
+    line_number = docs[0].insertText(point=fitz.Point(72, y), text=comment, fontsize=comment_font)
+
+    # x, y = 0, 36
+    y += ceil(line_number / 2) * 36
+    toc_y = y
     for bookmark in bookmarks:
         section, page = bookmark
-        toc.cell(w=x, h=y, txt=section, border=1, ln=1)
-    toc.output('toc.pdf', 'F')
+        # text = '-' * 140
+        # text = ' '.join([section, '-' * (100 - len(section)), str(page + 1)])
+        docs[0].insertText(point=fitz.Point(36, y), text=section)
+        docs[0].insertText(point=fitz.Point(540, y), text=str(page + 1))
+        y += 36
 
-    merger.append('toc.pdf')
-    for file in merge_list:
-        merger.append(file, import_bookmarks=False)
-
-    if os.path.exists('image.pdf'):
-        merger.append('image.pdf')
-
-    merger.write(merged_file)
-    merger.close()
+    docs.save(merged_file)
+    docs.close()
 
     for file in converted_files:
         os.remove(file)
 
-    os.remove('toc.pdf')
-    if os.path.exists('image.pdf'):
-        os.remove('image.pdf')
-
-    newWidth, newHeight = 612, 792
-
     reader, writer = PdfFileReader(merged_file), PdfFileWriter()
     for page in range(reader.numPages):
         page = reader.getPage(page)
-
-        page.scaleTo(newWidth, newHeight)
-
+        page.scaleTo(width=width, height=height)
         writer.addPage(page)
 
     with open("scaled.pdf", "wb") as f:
@@ -160,14 +194,13 @@ def main():
     pdf_file = PdfFileReader(open_file)
 
     writer = PdfFileWriter()
-
     for i in range(pdf_file.getNumPages()):
         writer.addPage(pdf_file.getPage(i))
 
-    xll, yll, xur, yur = 29, 763, 584, 727
-
+    xll, xur, yur = 36, 576, 792 - toc_y
+    yll = yur + 18
     for bookmark in bookmarks:
-        section, page = bookmark
+        _, page = bookmark
         writer.addLink(pagenum=0, pagedest=page, rect=[xll, yll, xur, yur])
         yll -= 36
         yur -= 36
--- readme.txt
+++ readme.txt
@@ -1,12 +1,14 @@
 Download PyMuPDF wheel for installed python version and platform from
-https://pypi.org/project/PyMuPDF/#files
+    https://pypi.org/project/PyMuPDF/#files
 and install like following
-pip install PyMuPDF-1.18.0-cp38-cp38-win_amd64.whl
+    pip install PyMuPDF-1.18.0-cp38-cp38-win_amd64.whl
 
 install other requirements via
-pip install -r requirements.txt
+    pip install -r requirements.txt
 
-then
-python main.py "PDF Working Data\\Sample 2"
+python main.py --folder ".\\PDF Working Data\\Sample 1" --logo logo.png --title "Complete Subscription Package" \
+    --sub_title "As Of October 9, 2020" --comment "This is a paragraph which should appear after the sub-title,
+    but before the start of the links/items in the table of contents"
 
-By default, it will process files in Sample 1 directory and produce merged pdf file named 'Sample 1.pdf'(directory name)
+By default, it will process files in Sample 2 directory and and produce merged pdf file named 'Sample 2.pdf'(directory
+name) with above parameters.
